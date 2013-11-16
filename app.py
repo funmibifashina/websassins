6377ff1fe794b20f6847e727a068871041c540fd
Fuyemi <Funmibi_Fashina@baylor.edu>
2013-11-15 22:42:25 -0600

    Add new pages

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ This is the application's main module.
 """
 
 import web              # A simple-looking Python HTTP framework I just found
-  
+
 # The URL structure of the entire application.
 # A feature of the web.py framework.
 # Syntax: 'regular expression', 'class to be called'
 urls = (
-  '/',                  'index',
-  '/death',        'death',
+    '/',             'index',
+    '/createdeath',  'createdeath',
+    '/death',        'death',
+    '/target',        'target',
 )
 
 # Tell web.py where to look to find page templates
@@ -19,16 +21,23 @@ render = web.template.render('templates/');
 
 # Classes that handle URLs
 class index:
-  def GET(self):
-    return render.index()
+    def GET(self):
+        return render.index()
+
+class createdeath:
+    def GET(self):
+        return render.createdeath()
 
 class death:
-  def GET(self):
-    return render.death()
+    def GET(self):
+        return render.death()
+
+class target:
+    def GET(self):
+        return render.target()
 
 # Initialize the application
 if __name__ == "__main__":
-  web.internalerror = web.debugerror
-  app = web.application(urls, globals())
-  app.run()
-
+    web.internalerror = web.debugerror
+    app = web.application(urls, globals())
+    app.run()
