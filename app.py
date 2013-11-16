2d2264a551231f065e373395bce6c9bfbb6f3215
Samuel Taylor <github@samueltaylor.org>
2013-11-15 22:36:10 -0600

    will send a text when page visited

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ app
 This is the application's main module.
 """
 
-import web              # A simple-looking Python HTTP framework I just found
-  
+import web
+from twilio.rest import TwilioRestClient
+import twiliocreds
+
 # The URL structure of the entire application.
 # A feature of the web.py framework.
 # Syntax: 'regular expression', 'class to be called'
 urls = (
-  '/',                  'index',
-  '/death',        'death',
+    '/',              'index',
+    '/death',         'death',
+    '/twilTest',      'twilTest',
 )
 
 # Tell web.py where to look to find page templates
@@ -19,16 +22,25 @@ render = web.template.render('templates/');
 
 # Classes that handle URLs
 class index:
-  def GET(self):
-    return render.index()
+    def GET(self):
+        return render.index()
 
 class death:
-  def GET(self):
-    return render.death()
+    def GET(self):
+        return render.death()
+
+class twilTest:
+    def GET(self):
+        client = TwilioRestClient(twiliocreds.account_sid,
+                twiliocreds.auth_token)
+        message = client.messages.create(to=twiliocreds.sams_phone,
+                from_="+16616674124",
+                body="Test")
+        return render.twilTest(str(message.sid))
 
 # Initialize the application
 if __name__ == "__main__":
-  web.internalerror = web.debugerror
-  app = web.application(urls, globals())
-  app.run()
+    web.internalerror = web.debugerror
+    app = web.application(urls, globals())
+    app.run()
 
